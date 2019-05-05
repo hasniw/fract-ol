ae541f890c510b2c8f40f6517f440a07de167222
Wassim <wassim.hasni42@gmail.com>
2019-05-05 18:17:38 +0200

    Quelques modif dans actions

--- srcs/ft_action.c
+++ srcs/ft_action.c
@@ -6,7 +6,7 @@
 /*   By: wahasni <wahasni@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/02 06:23:15 by wahasni           #+#    #+#             */
-/*   Updated: 2019/05/05 06:58:19 by wahasni          ###   ########.fr       */
+/*   Updated: 2019/05/05 18:17:17 by wahasni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,13 @@
 void	move(int key, t_args *args)
 {
 	if (key == KEY_LEFT)
-		args->x1 += 30 / args->zoom;
+		args->x1 += 25 / args->zoom;
 	if (key == KEY_RIGHT)
-		args->x1 -= 30 / args->zoom;
+		args->x1 -= 25 / args->zoom;
 	if (key == KEY_DOWN)
-		args->y1 -= 30 / args->zoom;
+		args->y1 -= 25 / args->zoom;
 	if (key == KEY_UP)
-		args->y1 += 30 / args->zoom;
+		args->y1 += 25 / args->zoom;
 }
 
 void	zoom(int x, int y, t_args *args)
